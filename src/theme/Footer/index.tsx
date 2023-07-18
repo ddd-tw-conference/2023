43959cc5908a751b3ace87f6126143789bb54373
Elvina <qq861924@gmail.com>
2023-07-18 13:52:44 +0800

    fix: 🐛 home layout banner

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,10 +1,7 @@
 /** @jsx jsx */
-import Link from "@docusaurus/Link";
 import { useLocation } from "@docusaurus/router";
 import type { WrapperProps } from "@docusaurus/types";
 import { jsx } from "@emotion/react";
-import { Box } from "@mui/joy";
-import { FocusButton } from "@site/src/components/FocusButton";
 import type FooterType from "@theme/Footer";
 import { Contacts } from "./Contacts";
 
@@ -18,18 +15,7 @@ export default function FooterWrapper(_props: Props): JSX.Element {
       <div className="container container-fluid">
         {location.pathname.endsWith("/sponsor") ||
         location.pathname.endsWith("/sponsor/") ? null : (
-          <div className="row ">
-            <FocusButton component={Link} to="sponsor">
-              <Box
-                sx={{
-                  fontSize: "1.5em",
-                }}
-              >
-                Join Us
-              </Box>
-              <Box>as a Sponsor</Box>
-            </FocusButton>
-          </div>
+          <div className="row "></div>
         )}
         <div className="row footer__links">
           <Contacts />
