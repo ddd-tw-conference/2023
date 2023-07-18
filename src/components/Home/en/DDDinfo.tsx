2ea015ffe8e7e2cb50d2e10e7ed5a67dfa544ac0
chicken <hibary.taiwan@gmail.com>
2023-07-18 20:50:56 +0800

    feat: en ddd info

---
+++ src/components/Home/en/DDDinfo.tsx
@@ -0,0 +1,88 @@
+/** @jsx jsx */
+import Link from "@docusaurus/Link";
+import { css, jsx } from "@emotion/react";
+import DDDlogo from "@site/src/assets/ddd.png";
+import { memo } from "react";
+
+const DDDinfo = memo(function DDDinfo() {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        gap: 48px;
+        background: #766d94;
+        padding: 60px;
+        justify-content: center;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          gap: 24px;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <img
+          alt="ddd-logo"
+          src={DDDlogo}
+          css={css`
+            width: 172px;
+            height: 162px;
+          `}
+        />
+        <p>
+          DDDTW community is dedicated to promoting the widely adopted
+          methodology of Domain-Driven Design in Taiwan. Over the past five
+          years, we have organized hundreds of events throughout Taiwan,
+          attracting thousands of people to understand and practice DDD. We are
+          thrilled to announce that the 3rd DDDesign TW Annual Conference will
+          be held on September 16, 2023, at the Social Sciences Institute of
+          National Taiwan University. This year's theme is ""Inspiration,""
+          aiming to inspire participants and drive meaningful changes through
+          firsthand practical experiences. In our pursuit of better event
+          quality, we sincerely invite your company to sponsor this year's
+          annual conference. Join us in this grand celebration of the software
+          industry! The event is expected to attract nearly 500 software
+          professionals and feature 13 keynote speeches and 3 workshops, with
+          speakers including practitioners and consultants from Taiwan, DDD_EU,
+          and DDD China. We welcome your sponsorship to propel businesses
+          forward with technology!
+        </p>
+      </div>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <button
+          css={css`
+            border-radius: 4px;
+            padding: 9px 22px;
+            background: #9ad7eb;
+            color: #242526;
+            font-size: 16px;
+            font-weight: 600;
+          `}
+        >
+          <Link
+            to="/sponsor"
+            css={css`
+              color: #242526;
+              :hover {
+                color: #242526;
+                text-decoration: none;
+              }
+            `}
+          >
+            Join Us as a Sponsor
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+});
+
+export { DDDinfo };
