06afbc445090f51a054b6d74a236a5cc446f0f95
chicken <hibary.taiwan@gmail.com>
2023-07-18 21:01:25 +0800

    feat: en info

--- src/components/Home/en/Info.tsx
+++ src/components/Home/en/Info.tsx
@@ -19,7 +19,7 @@ const Info = memo(function Info() {
           font-weight: 600;
         `}
       >
-        「 不單只是吸收知識，更要創造連結與獲得啟發！」
+        "Beyond mere knowledge absorption, it is crucial to establish connections and seek inspiration!"
       </p>
       <div
         css={css`
@@ -27,23 +27,31 @@ const Info = memo(function Info() {
         `}
       >
         <p>
-          DDD 自 Eric Evans 於 2003
-          年出版第一本書以來，已深刻影響全球軟體業界超過 20 年。
-          隨着業務需求複雜化與微服務架構的熱潮， DDD
-          也成爲各種技術與軟體設計的指導原則。
+          DDD, since the publication of Eric Evans' first book in 2003, has
+          profoundly influenced the global software industry for over 20 years.
+          With the increasing complexity of business requirements and the rise
+          of microservices architecture, DDD has become a guiding principle in
+          various technical and software design practices.
         </p>
 
         <p>
-          在技術實踐以外， DDD 更關乎團隊文化與心態的轉變:如何實現業務與 IT
-          的共同目標、如何讓技術承接上業務驅動的改變，甚至讓技術帶領業務前進！
+          Beyond technical implementation, DDD also encompasses a cultural and
+          mindset shift within teams: how to align business and IT goals, enable
+          technology to accommodate business-driven changes, and even allow
+          technology to lead the way for the business.
         </p>
 
         <p>
-          今年主題爲「啟發」，我們希望不同背景與專業的人士能打破藩籬來互相交流，並激發出思想的火花。
+          This year's theme is "Inspiration," aiming to break down barriers and
+          foster exchanges among professionals from diverse backgrounds and
+          fields, sparking new ideas.
         </p>
 
         <p>
-          因此，我們將邀請國內外的領域驅動設計實踐家來分享經驗，希望觀衆不論程度都能從演講、互動環節、工作坊中獲得啟發並產生改變。
+          Therefore, we will invite both domestic and international experts in
+          Domain-Driven Design to share their experiences. We hope that
+          attendees at all levels can be inspired and bring about meaningful
+          changes through talks, interactive sessions, and workshops.
         </p>
       </div>
     </div>
