7231a8de149394483160c2db774c20c687f96a9e
Elvina <qq861924@gmail.com>
2023-07-25 14:06:19 +0800

    feat: 🎸 program page

---
+++ src/pages/program.tsx
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+import { Program } from "@site/src/layout/Program";
+import Layout from "@theme/Layout";
+import { createElement } from "react";
+
+export default function Page(): JSX.Element {
+  return (
+    <Layout>
+      <Program
+        content={createElement(
+          require("@site/src/content/en/program.mdx").default
+        )}
+      />
+    </Layout>
+  );
+}
