08e3b050081b3923fcf7adf9bdd8db17eaf06395
Elvina <qq861924@gmail.com>
2023-08-06 22:01:37 +0800

    feat: 🎸 link to speaker

--- src/layout/Speaker.tsx
+++ src/layout/Speaker.tsx
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import Link from "@docusaurus/Link";
 import { jsx } from "@emotion/react";
 import { Card, Stack, Typography } from "@mui/joy";
 import thumbnail from "@site/src/assets/program-banner.png";
@@ -14,43 +15,50 @@ const Program = memo(function Program({
   speakers: programer_type[];
 }) {
   return (
-    <Stack
-      className="container"
-      sx={{
-        gap: "2rem",
-        mb: "5rem",
-      }}
-    >
-      <Image img={thumbnail} />
-
-      <Typography level="h2" fontSize="md" textAlign="center" sx={{ mb: 0.5 }}>
-        {content}
-      </Typography>
-
+    <Link>
       <Stack
-        direction="row"
-        spacing={2}
-        useFlexGap
-        flexWrap="wrap"
-        justifyContent="flex-start"
+        className="container"
+        sx={{
+          gap: "2rem",
+          mb: "5rem",
+        }}
       >
-        {PROGRAMER.map((programer, index) => {
-          return (
-            <Card key={index} variant="outlined" sx={{ width: 300 }}>
-              <Image img={programer.photo} />
-              <Typography
-                level="h2"
-                fontSize="md"
-                textAlign="center"
-                sx={{ mb: 0.5 }}
-              >
-                {speakers[index].name}
-              </Typography>
-            </Card>
-          );
-        })}
+        <Image img={thumbnail} />
+
+        <Typography
+          level="h2"
+          fontSize="md"
+          textAlign="center"
+          sx={{ mb: 0.5 }}
+        >
+          {content}
+        </Typography>
+
+        <Stack
+          direction="row"
+          spacing={2}
+          useFlexGap
+          flexWrap="wrap"
+          justifyContent="flex-start"
+        >
+          {PROGRAMER.map((programer, index) => {
+            return (
+              <Card key={index} variant="outlined" sx={{ width: 300 }}>
+                <Image img={programer.photo} />
+                <Typography
+                  level="h2"
+                  fontSize="md"
+                  textAlign="center"
+                  sx={{ mb: 0.5 }}
+                >
+                  {speakers[index].name}
+                </Typography>
+              </Card>
+            );
+          })}
+        </Stack>
       </Stack>
-    </Stack>
+    </Link>
   );
 });
 
