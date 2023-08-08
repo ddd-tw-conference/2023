00b9741885edf683d87962de8823dd3b56d4d7ce
Elvina <qq861924@gmail.com>
2023-08-08 15:03:07 +0800

    fix: 🐛 speaker style

--- src/layout/Speaker.tsx
+++ src/layout/Speaker.tsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/react";
-import { Stack, Typography } from "@mui/joy";
-import thumbnail from "@site/src/assets/speaker-banner.png";
-import { programer_type } from "@site/src/model/Speakers";
-import Image from "@theme/IdealImage";
-import { ReactNode, memo } from "react";
+import { jsx } from '@emotion/react';
+import { Stack, Typography } from '@mui/joy';
+import thumbnail from '@site/src/assets/speaker-banner.png';
+import { programer_type } from '@site/src/model/Speakers';
+import Image from '@theme/IdealImage';
+import { ReactNode, memo } from 'react';
 
 const Speaker = memo(function Speaker({
   content,
@@ -17,47 +17,50 @@ const Speaker = memo(function Speaker({
     <Stack
       className="container"
       sx={{
-        gap: "2rem",
-        mb: "5rem",
+        gap: '2rem',
+        mb: '5rem',
       }}
     >
       <Image img={thumbnail} />
 
       <Stack
         sx={{
-          bgcolor: "rgba(118, 109, 148, 0.40)",
-          borderRadius: "4px",
-          padding: "60px",
-          display: "flex",
-          flexDirection: "row",
-          gap: "23px",
-          flexWrap: "wrap",
+          bgcolor: 'rgba(118, 109, 148, 0.40)',
+          borderRadius: '4px',
+          padding: '60px',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '23px',
+          flexWrap: 'wrap',
         }}
       >
         <Stack
           sx={{
-            width: "276px",
-            height: "220px",
-            objectFit: "contain",
+            width: '276px',
+            height: '220px',
+            objectFit: 'contain',
           }}
         >
           <Image img={speaker.photo} />
         </Stack>
         <Stack
           sx={{
-            display: "flex",
-            flexDirection: "column",
+            display: 'flex',
+            flexDirection: 'column',
+            width: 'calc(100% - 359px)',
+            gap: '12px',
           }}
         >
-          <Typography
-            level="h2"
-            fontSize="md"
-            textAlign="left"
-            sx={{ mb: 0.5 }}
-          >
+          <Typography level="h4" fontSize="md" textAlign="left" sx={{ mb: 0.5 }}>
             {speaker.name}
           </Typography>
-          <Stack>{content}</Stack>
+          <Stack
+            sx={{
+              fontSize: '1rem',
+            }}
+          >
+            {content}
+          </Stack>
         </Stack>
       </Stack>
     </Stack>
