eee7b8f9e1f9e7d4556bfb40a6ee127eda2342dc
VdustR <vdustr@gmail.com>
2023-08-20 14:38:17 +0800

    fix: better speakers

--- src/layout/Speakers.tsx
+++ src/layout/Speakers.tsx
@@ -15,62 +15,69 @@ const Speakers = memo(function Speakers({
   speakers: programer_type[];
 }) {
   return (
-    <Link>
+    <Stack
+      className="container"
+      sx={{
+        gap: "2rem",
+        mb: "5rem",
+      }}
+    >
+      <Image img={thumbnail} />
+
+      <Typography level="h2" fontSize="md" textAlign="center" sx={{ mb: 0.5 }}>
+        {content}
+      </Typography>
+
       <Stack
-        className="container"
-        sx={{
-          gap: "2rem",
-          mb: "5rem",
-        }}
+        direction="row"
+        spacing={2}
+        useFlexGap
+        flexWrap="wrap"
+        justifyContent="flex-start"
       >
-        <Image img={thumbnail} />
-
-        <Typography
-          level="h2"
-          fontSize="md"
-          textAlign="center"
-          sx={{ mb: 0.5 }}
-        >
-          {content}
-        </Typography>
-
-        <Stack
-          direction="row"
-          spacing={2}
-          useFlexGap
-          flexWrap="wrap"
-          justifyContent="flex-start"
-        >
-          {speakers.map((speaker, index) => {
-            return (
-              <Card key={index} variant="outlined" sx={{ width: 300 }}>
-                <div
-                  css={css`
-                    width: 276px;
-                    height: 220px;
-                    object-fit: contain;
-                    display: flex;
-                    justify-content: center;
-                  `}
-                >
-                  <Link to={`/speaker/${speaker.slug}`}>
-                    <Image img={speaker.photo} />
-                  </Link>
-                </div>
-                <Typography
-                  level="h2"
-                  fontSize="md"
-                  textAlign="center"
-                  sx={{ mb: 0.5 }}
-                >
-                  {speaker.name}
-                </Typography>
-              </Card>
-            );
-          })}
-        </Stack>
+        {speakers.map((speaker, index) => {
+          return (
+            <Card
+              key={index}
+              variant="outlined"
+              sx={{ width: 300 }}
+              component={Link}
+              to={`/speaker/${speaker.slug}`}
+              css={css`
+                transition: transform 0.3s ease;
+                text-decoration: none;
+                &:hover {
+                  transform: scale(1.05);
+                  text-decoration: none;
+                  font-weight: 700;
+                  color: var(--ifm-color-primary);
+                }
+              `}
+            >
+              <div
+                css={css`
+                  width: 276px;
+                  height: 220px;
+                  object-fit: contain;
+                  display: flex;
+                  justify-content: center;
+                `}
+              >
+                <Image img={speaker.photo} />
+              </div>
+              <Typography
+                level="h2"
+                fontSize="md"
+                textAlign="center"
+                sx={{ mb: 0.5, color: "inherit" }}
+              >
+                {speaker.name}
+              </Typography>
+            </Card>
+          );
+        })}
       </Stack>
-    </Link>
+    </Stack>
   );
 });
 
